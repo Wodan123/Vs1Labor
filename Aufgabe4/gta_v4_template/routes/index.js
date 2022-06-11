e2e2ed7a7ff42506260de66602ddb2bdf2e7cb80
Maximilian-prog <maximilian.buecher@gmx.de>
2022-06-11 13:08:56 +0200

    Aufgabe 4.1.1 done
    Vorherige Lösungen übernehmen ist fertig.

--- Aufgabe4/gta_v4_template/routes/index.js
+++ Aufgabe4/gta_v4_template/routes/index.js
@@ -19,6 +19,7 @@ const router = express.Router();
  */
 // eslint-disable-next-line no-unused-vars
 const GeoTag = require('../models/geotag');
+const GeoTagExamples = require("../models/geotag-examples");
 
 /**
  * The module "geotag-store" exports a class GeoTagStore. 
@@ -26,7 +27,9 @@ const GeoTag = require('../models/geotag');
  */
 // eslint-disable-next-line no-unused-vars
 const GeoTagStore = require('../models/geotag-store');
+var GeoTagStoreObject = new GeoTagStore();
 
+GeoTagStoreObject.examples(); //Lade die Geotag-Beispiele in unser Array
 // App routes (A3)
 
 /**
@@ -40,6 +43,61 @@ const GeoTagStore = require('../models/geotag-store');
 
 router.get('/', (req, res) => {
   res.render('index', { taglist: [] })
+});
+
+/**
+ * Route '/tagging' for HTTP 'POST' requests.
+ * (http://expressjs.com/de/4x/api.html#app.post.method)
+ *
+ * Requests cary the fields of the tagging form in the body.
+ * (http://expressjs.com/de/4x/api.html#req.body)
+ *
+ * Based on the form data, a new geotag is created and stored.
+ *
+ * As response, the ejs-template is rendered with geotag objects.
+ * All result objects are located in the proximity of the new geotag.
+ * To this end, "GeoTagStore" provides a method to search geotags 
+ * by radius around a given location.
+ */
+
+// TODO: ... your code here ...
+router.post('/tagging',(req, res)=> {
+  GeoTagStoreObject.addGeoTag(new GeoTag(req.body.tagname, req.body.tagLatitude, req.body.tagLongitude, req.body.taghashtag));
+  let nearbyGT = GeoTagStoreObject.getNearbyGeoTags(req.body.tagLatitude, req.body.tagLongitude);
+  console.log(req.body);
+    res.render("index", { 
+      taglist: nearbyGT,
+      latvalue: req.body.tagLatitude,
+      longvalue: req.body.tagLongitude, 
+      mapGeoTagList: JSON.stringify(nearbyGT)
+    });   
+})
+/**
+ * Route '/discovery' for HTTP 'POST' requests.
+ * (http://expressjs.com/de/4x/api.html#app.post.method)
+ *
+ * Requests cary the fields of the discovery form in the body.
+ * This includes coordinates and an optional search term.
+ * (http://expressjs.com/de/4x/api.html#req.body)
+ *
+ * As response, the ejs-template is rendered with geotag objects.
+ * All result objects are located in the proximity of the given coordinates.
+ * If a search term is given, the results are further filtered to contain 
+ * the term as a part of their names or hashtags. 
+ * To this end, "GeoTagStore" provides methods to search geotags 
+ * by radius and keyword.
+ */
+
+// TODO: ... your code here ...
+router.post('/discovery',(req, res)=> {
+  let searchTerm = req.body.searchbox;
+  let searching = GeoTagStoreObject.searchNearbyGeoTags(searchTerm);
+  res.render("index", { 
+    taglist: searching,
+    latvalue: req.body.hiddenLatitude,
+    longvalue: req.body.hiddenLongitude,
+    mapGeoTagList: JSON.stringify(searching)
+  });   
 });
 
 // API routes (A4)
